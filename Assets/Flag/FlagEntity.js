162fc676a7586d9fd2dbf168df7d2caa3e7bb8ef
Gerard Llorach <gerard.llorach@gmail.com>
2022-08-26 13:05:32 +0200

    loaded flag file

---
+++ Assets/Flag/FlagEntity.js
@@ -0,0 +1,54 @@
+import * as THREE from 'three';
+import { GLTFLoader } from 'https://threejs.org/examples/jsm/loaders/GLTFLoader.js';
+//import { WindsockBehavior } from '/OBSEA/Assets/Windsock/WindsockBehavior.js'
+
+class FlagEntity {
+  
+  isLoaded = false;
+  windIntensity;
+  windDirection;
+  time = 0;
+  
+  constructor(scene, onload){
+
+    // Initial variables
+    this.windIntensity = 10; // km/h
+    this.windDirection = 0; // degrees
+
+    const gltfLoader = new GLTFLoader();
+    // objLoader.load('https://threejs.org/manual/examples/resources/models/windmill/windmill.obj', (root) => {
+    gltfLoader.load('/OBSEA/Assets/Flag/flag.glb', (gltf) => {
+      // GLTF scene
+      this.root = gltf.scene;
+  
+      // Fix frustrum culling
+      debugger;
+      //this.root.children[0].children[1].frustumCulled = false;
+
+      // Create Windsock Behavior
+      //this.windsockBehavior = new WindsockBehavior(this.root, scene);
+
+      scene.add(this.root);
+
+      this.isLoaded = true;
+
+      if (onload)
+        onload();
+    });
+
+  }
+
+  // UPDATE
+  update(time){
+    this.time = time; // TODO: USE dt instead of time. Should also change the function inside windsockBehavior
+    //this.windsockBehavior.updateWindSock(this.root, this.windIntensity, this.windDirection, this.time);
+  }
+
+  // Set wind parameters
+  setWindParams(windIntensity, windDirection){
+    this.windIntensity = windIntensity;
+    this.windDirection = windDirection;
+  }
+}
+
+export { FlagEntity }
