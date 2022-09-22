e354d75526bf831658ef7fb1457ff6b8751c5667
Gerard Llorach <gerard.llorach@gmail.com>
2022-09-22 13:14:38 +0200

    forgot spanish translation stage

--- lang/es.js
+++ lang/es.js
@@ -1,7 +1,7 @@
 let es;
 export default es = {
   "buoyButtonTitle": "Centra la cámara en la boya de superfície",
-  "buoyButtonTitle": "Centra la cámara en el observatorio submarino",
+  "baseButtonTitle": "Centra la cámara en el observatorio submarino",
   "Wind": "Viento",
   "Waves": "Oleaje"
 }
