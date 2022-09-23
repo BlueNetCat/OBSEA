6a8fda8d3f1a6cfb6207a029e6be45501e857626
Gerard Llorach <gerard.llorach@gmail.com>
2022-09-23 15:43:11 +0200

    new icons for bottom section

--- Components/Bottom/BottomSection.vue
+++ Components/Bottom/BottomSection.vue
@@ -1,15 +1,38 @@
 <template>
-  <div id="bottom-section">
+  <div id="bottom-section" style="margin-right: 10px">
 
     <!-- Bottom icons -->
     <!-- Above water -->
     <div class="bottom-icons" v-show="!isUnderwater">
-      <button>Wave configuration icon</button>
-      <button>Wind configuration icon</button>
+      <button>
+        <svg class="icon-svg" xmlns="http://www.w3.org/2000/svg" width="512" height="512" viewBox="0 0 512 512">
+            <path id="Wave" class="svg-wavePath"
+              d="M91,345a148.964,148.964,0,0,0,39-34c18.237-22.738,21.847-41.833,31.9-66.57C180.68,198.242,210.248,167.907,225,159c24.989-15.088,68.213-28.479,112-10,45.368,19.146,74.013,67.228,65,79-10.978,14.338-66.772-22.893-88-2-16.914,16.647-8.635,64.768,21,90,17.036,14.5,39.538,20.066,62,18" />
+        </svg>
+      </button>
+      <button>
+        <svg class="icon-svg" xmlns="http://www.w3.org/2000/svg" width="512" height="512" viewBox="0 0 512 512">
+          <path id="WindTop" class="svg-windPath"
+            d="M117,178c96.486,32.553,201.682,46.769,260,0,20.145-16.156,26.414-30.873,26-41-1.07-26.169-36.825-52.812-63-45-9.183,2.741-23.774,11.71-23,25,0.6,10.338,10.406,20.516,25,23" />
+          <path id="WindMiddle" class="svg-windPath"
+            d="M64.388,323.7c66.094,22.349,138.154,32.108,178.1,0,13.8-11.092,18.094-21.2,17.81-28.148-0.733-17.966-25.226-36.257-43.155-30.894-6.291,1.882-16.286,8.04-15.756,17.163,0.413,7.1,7.129,14.085,17.126,15.791" />
+          <path id="WindBottom" class="svg-windPath"
+            d="M149,391c96.486,32.553,201.682,46.769,260,0,20.145-16.156,26.414-30.873,26-41-1.07-26.169-36.825-52.812-63-45-9.183,2.741-23.774,11.71-23,25,0.6,10.338,10.406,20.516,25,23" />
+        </svg>
+
+      </button>
     </div>
     <!-- Underwater -->
     <div class="bottom-icons" v-show="isUnderwater">
-      <button @click="cameraButtonClicked">Camera youtube icon</button>
+      <button @click="cameraButtonClicked">
+        <svg class="icon-svg" xmlns="http://www.w3.org/2000/svg" width="512" height="512" viewBox="0 0 512 512">
+          <path id="Rounded_Rectangle_1" data-name="Rounded Rectangle 1" class="cls-1"
+            d="M209.385,243.21H396a10,10,0,0,1,10,10V355.955a10,10,0,0,1-10,10H209.385a10,10,0,0,1-10-10V253.21A10,10,0,0,1,209.385,243.21Z" />
+          <path class="cls-1" d="M100,225.5l98.968,44.811v63.764L101.62,373.99Z" />
+          <circle class="cls-2" cx="242.078" cy="190" r="55.047" />
+          <circle class="cls-2" cx="352.953" cy="190" r="55.047" />
+        </svg>
+      </button>
     </div>
 
     <!-- Underwater camera -->
@@ -70,5 +93,44 @@ export default {
   display: flex;
   flex-direction: column;
   align-items: flex-end;
+  padding: 10px;
+}
+
+button {
+  border-style: none;
+  padding: 0;
+  margin-bottom: 10px;
+  border-radius: 50%;
+}
+
+
+
+/* Wave SVG */
+.svg-wavePath {
+  fill: none;
+  stroke: #1a1a1a;
+  stroke-width: 20px;
+  fill-rule: evenodd;
+}
+
+/* Wind SVG */
+.svg-windPath {
+  fill: none;
+  stroke: #1a1a1a;
+  stroke-width: 20px;
+  fill-rule: evenodd;
+}
+
+
+/* Camera SVG */
+.cls-1,
+.cls-2 {
+  fill: none;
+  stroke: #1a1a1a;
+  stroke-width: 25px;
+}
+
+.cls-2 {
+  fill-rule: evenodd;
 }
 </style>
